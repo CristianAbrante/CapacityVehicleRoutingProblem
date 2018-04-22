f08c3127ebc8c6312d9e54a9d37b55f46cf33845
Carlos Domínguez García <carlosdg97@gmail.com>
2018-04-22 17:43:02 +0100

    Feat: utils
    Added utility class for double type numbers comparison

---
+++ src/daa/project/crvp/utils/DoubleCompare.java
@@ -0,0 +1,27 @@
+package daa.project.crvp.utils;
+
+/**
+ * Utility class for comparison of doubles
+ * 
+ * @author Carlos Dominguez Garcia (alu0100966589)
+ * @version 1.0.0
+ * @since 1.0.0 (Apr 22, 2018)
+ * @file DoubleCompare.java
+ *
+ */
+public class DoubleCompare {
+    /** Epsilon: threshold value. We will accept comparisons as true with this as error margin*/
+    public static final double EPSILON = 10E-6;
+    
+    /**
+     * Returns whether a is less than b or not.
+     * 
+     * @param a First value
+     * @param b Second value
+     * @return Whether a is less than b or not.
+     */
+    public static boolean lessThan(double a, double b) {
+        return (a - b) < -EPSILON;
+    }
+    
+}

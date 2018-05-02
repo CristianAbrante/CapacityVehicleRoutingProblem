ff0bf33f80bf53f83111f6bf14980a445f2f1c63
Ángel Igareta <alu0100967111@ull.edu.es>
2018-05-02 11:30:52 -0700

    Added options to select the solution generator and algorithm in main.

--- src/daa/project/crvp/CVRPMain.java
+++ src/daa/project/crvp/CVRPMain.java
@@ -7,6 +7,7 @@ import daa.project.crvp.IO.ReaderFromFile;
 import daa.project.crvp.algorithms.GRASP;
 import daa.project.crvp.algorithms.Multiboot;
 import daa.project.crvp.algorithms.VariableNeighborhoodSearch;
+import daa.project.crvp.graphic.CVRPGraphic;
 import daa.project.crvp.local_search.BestNeighborLocalSearch;
 import daa.project.crvp.local_search.LocalSearch;
 import daa.project.crvp.local_search.TabuSearch;
@@ -35,9 +36,11 @@ public class CVRPMain {
 			throw new IllegalArgumentException("You must specify the CRVP problem specification file. ");
 		}
 
+		// READ PROBLEM
 		ReaderFromFile reader = new ReaderFromFile(args[0]);
 		CVRPSpecification problemSpecification = reader.getProblemSpecification();
 
+		// SHOW PROBLEM INFO
 		System.out.println("Depot ID: " + problemSpecification.getDepotID());
 		System.out.println("Capacity: " + problemSpecification.getCapacity());
 		System.out.println("Client Number: " + problemSpecification.getClients().size());
@@ -52,54 +55,84 @@ public class CVRPMain {
 
 		System.out.println("Total Demand: " + totalDemand);
 
-		//
-		// Algorithms tests
-		//
-
+		// TEST ALGORITHMS
 		TimeAndIterationsRecorder multibootRecorder = new TimeAndIterationsRecorder();
-
 		Move[] moveList = new Move[] { new InterrouteSwap(), new Relocation(), new IntrarouteSwap(), new TwoOpt() };
+
+		/** SOLUTION CHOOSER */
+		CVRPSolution solution = Multiboot.constructRandomSolution(problemSpecification);
+
+		int choosenSolutionGenerator = 2;
+		Move choosenMove = new Relocation();
+		switch (choosenSolutionGenerator) {
+			case 0: // Grasp
+				System.out.println("\t*** SOLUTION GENERATOR -> GRASP ***");
+				solution = GRASP.grasp(problemSpecification, 100, 100, 3, new BestNeighborLocalSearch(choosenMove));
+				System.out.println("Grasp. Initial solution total distance: " + solution.getTotalDistance());
+				break;
+			case 1: // Multiboot
+				System.out.println("\t*** SOLUTION GENERATOR -> MULTIBOOT ***");
+				solution = Multiboot.multiboot(problemSpecification, new BestNeighborLocalSearch(choosenMove), 100,
+						multibootRecorder);
+				System.out.println("Multiboot. Initial solution total distance: " + solution.getTotalDistance());
+				System.out.println(multibootRecorder.toString());
+				break;
+			case 2: // Multiboot random
+				System.out.println("\t*** SOLUTION GENERATOR -> MULTIBOOT RANDOM ***");
+				solution = Multiboot.constructRandomSolution(problemSpecification);
+				System.out.println("Random solution. Initial solution total distance: " + solution.getTotalDistance());
+				break;
+		}
+
 		LocalSearch vnd = new VariableNeighborhoodDescent(moveList);
 
-		// GRASP initial solution
-		// CVRPSolution solution = GRASP.grasp(problemSpecification, 100, 100, 3,
-		// new BestNeighborLocalSearch(new Relocation()));
-		// System.out.println("GRASP. Initial solution total distance: " +
-		// solution.getTotalDistance());
+		int tabuTenure = (int) (0.2 * problemSpecification.getClients().size());
+		boolean tabuVerbose = false;
+		Move[] tabuMoveList = moveList; // new Move[] {new Relocation(), new IntrarouteSwap()};
+		LocalSearch tabuSearch = new TabuSearch(tabuMoveList, tabuTenure, 10, tabuVerbose);
 
-		// Multiboot initial solution
-		// CVRPSolution solution = Multiboot.multiboot(problemSpecification, new
-		// BestNeighborLocalSearch(new Relocation()),
-		// 100, multibootRecorder);
-		// System.out.println("Multiboot. Initial solution total distance: " +
-		// solution.getTotalDistance());
-		// System.out.println(multibootRecorder.toString());
-		//
-		// // Random initial solution
-		//// CVRPSolution solution =
-		// Multiboot.constructRandomSolution(problemSpecification);
-		//// System.out.println("Random solution. Initial solution total distance: " +
-		// solution.getTotalDistance());
-		//
-		// for (int i = 0; i < 100; ++i) {
-		// solution = VariableNeighborhoodSearch.run(solution, moveList, vnd);
-		// }
-		// System.out.println("Is solution feasible after various runs of VNS?: " +
-		// solution.isFeasible());
-		// System.out.println("Total distance after various runs of VNS: " +
-		// solution.getTotalDistance());
-		// CVRPGraphic window = new CVRPGraphic();
-		// window.setSolution(solution);
-		// window.showSolution();
+		/** ALGORITHM CHOOSER */
+		int choosenAlgortihm = 2;
+		switch (choosenAlgortihm) {
+			case 0: // VNS
+				System.out.println("\t*** ALGORITHM USED -> VNS + VND ***");
 
-		CVRPSolution solution = Multiboot.multiboot(problemSpecification, new BestNeighborLocalSearch(new Relocation()),
-				100, multibootRecorder);
-//		CVRPSolution solution = Multiboot.constructRandomSolution(problemSpecification);
-		System.out.println("Multiboot. Initial solution total distance: " + solution.getTotalDistance());
-		
-		TabuSearch tabuSearch = new TabuSearch(new Move[] { new InterrouteSwap() }, 3, 1000);
-		solution = tabuSearch.findLocalOptimum(solution);
-		System.out.println("Total distance after various runs of Tabu Search: " + solution.getTotalDistance());
+				for (int i = 1; i <= 100; ++i) {
+					solution = VariableNeighborhoodSearch.run(solution, moveList, vnd);
+					//System.out.println("VNS Solution #" + i + ": " + solution.getTotalDistance());
+				}
+				System.out.println("Is solution feasible after various runs of VNS?: " + solution.isFeasible());
+				System.out.println("Total distance after various runs of VNS: " + solution.getTotalDistance());
+				break;
+			case 1: // VNS + TABU SEARCH
+				System.out.println("\t*** ALGORITHM USED -> VNS + TABU SEARCH ***");
+				System.out.println("Tabu Tenure:" + tabuTenure);
+
+				for (int i = 1; i <= 100; ++i) {
+					solution = VariableNeighborhoodSearch.run(solution, moveList, tabuSearch);
+					//System.out.println("Tabu Solution #" + i + ": " + solution.getTotalDistance());
+				}
+				System.out.println("Total distance after multiple runs of Tabu Search: " + solution.getTotalDistance());
+				break;
+			case 2: // TABU
+				System.out.println("\t*** ALGORITHM USED -> TABU SEARCH ***");
+				System.out.println("Tabu Tenure:" + tabuTenure);
+
+				for (int i = 1; i <= 100; ++i) {
+					solution = tabuSearch.findLocalOptimum(solution);
+					//System.out.println("Tabu Solution #" + i + ": " + solution.getTotalDistance());				
+				}
+				System.out.println("Total distance after multiple runs of Tabu Search: " + solution.getTotalDistance());
+			default:
+				break;
+		}
+
+		boolean verbose = true;
+		if (verbose) {
+			CVRPGraphic window = new CVRPGraphic();
+			window.setSolution(solution);
+			window.showSolution();
+		}
 	}
 
 }
